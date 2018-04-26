84bbdca18223f67723e56d732b89a57b4a07d878
Landon <laba2346@colorado.edu>
2018-04-26 17:51:30 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Pitching Average Calculator', pa: ''});
+});
+
+router.get('/search', function(req, res, next){
+    req.url = '/';
+    res.render('index', { title: 'Pitching Average Calculator', pa: ''} );
+});
+
+router.post('/search', function(req, res, next) {
+    console.log("Received search request for" + req.body.firstName + " " + req.body.lastName);
+    res.render('index', { title: 'Pitching Average Calculator', pa: 'Just searched!', firstName: req.body.firstName, lastName: req.body.lastName});
+});
+
+module.exports = router;
