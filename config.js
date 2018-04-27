b85c76be0154ff452c08676e5d584e639df9dcba
Landon <laba2346@colorado.edu>
2018-04-26 21:30:08 -0600

    meme

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    production: {
+        host: 'ec2-54-235-193-34.compute-1.amazonaws.com',
+        port: 5432,
+        database: 'dbkt5tfgdagmv7',
+        user: 'fhttwaphawybpt',
+        password: 'd888c67b12c6aaf6f7bcd61926302aa6d1b882d1a068adfb5419ec9da650b8f4'
+    }
+}
