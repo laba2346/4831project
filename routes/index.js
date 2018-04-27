75f62a92d13b08e918cfb605f7e8f8c59c6ef577
Landon <laba2346@colorado.edu>
2018-04-26 20:35:39 -0600

    neato

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var pgp = require('pg-promise');
-var db = pgp('postgres://fhttwaphawybpt:d888c67b12c6aaf6f7bcd61926302aa6d1b882d1a068adfb5419ec9da650b8f4@ec2-54-235-193-34.compute-1.amazonaws.com:5432/dbkt5tfgdagmv7');
-
+const cn = {
+    host: 'ec2-54-235-193-34.compute-1.amazonaws.com',
+    port: 5432,
+    database: 'dbkt5tfgdagmv7',
+    user: 'fhttwaphawybpt',
+    password: 'd888c67b12c6aaf6f7bcd61926302aa6d1b882d1a068adfb5419ec9da650b8f4'
+};
+var db = pgp(cn);
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Pitching Average Calculator', pa: ''});
